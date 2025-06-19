e74b3ccb716170ac99be19b5ddd0bd7edde6c184
Lowkey <113860192+56snty@users.noreply.github.com>
2025-06-19 10:03:16 +0200

    Migrations added (DB)

---
+++ wel_summamove_laravel/database/migrations/2025_06_19_075019_create_gebruikers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+public function up()
+{
+    Schema::create('gebruikers', function (Blueprint $table) {
+        $table->id();
+        $table->string('gebruikersnaam', 191)->unique(); // max lengte 191
+        $table->boolean('geregistreerd')->default(false);
+        $table->timestamps();
+    });
+}
+
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gebruikers');
+    }
+};
